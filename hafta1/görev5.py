46b9bacc0f4da8905518983944de1d63abbe68e2
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-08-24 07:31:48 +0300

    Implement gradient descent and helper loss calculation, finished

--- hafta1/görev5.py
+++ hafta1/görev5.py
@@ -1,3 +1,17 @@
-def gradient_descent
-  list = list +  gradient(list)
-  visualize(loss(list))
+list1 = [0.6, 0.76, 0.83, 1.2, 1.34]
+list2 = [0.5, 0.7, 0.9, 1.15, 1,46]
+def loss_calc (w, b):
+  pred = []
+  for x in X:
+    pred.append(x * w + b)
+  return loss_mse (pred, list2)
+
+def gradient_descent(w, b, lr, steps):
+  h = 0.000001
+  for i in range(steps):
+    slope_w = (loss_calc(w + h, b) - loss_calc(w, b)) / h
+    slope_b = (loss_calc(w, b + h) - loss_calc(w, b)) / h
+    print("Step: ", i, "loss=", loss_mse(w, b))
+    w = w -lr * slope_w
+    b = b -lr * slope_b
+  return w, b
