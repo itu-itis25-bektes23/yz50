65ede6ca1a1f63ce10d8b48edc8aec914e96d577
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-08-24 07:10:28 +0300

    Refactor loss function and add MSE calculation

--- hafta1/görev3.py
+++ hafta1/görev3.py
@@ -1,3 +1,12 @@
-def loss p
-  loss = - log p
+import math
+def loss_log(p)
+  loss = - math.log(p)
   return loss
+
+def loss_mse(predictions, targets)
+  total = 0
+  for predictions, targets in zip(predictions, targets):
+    diff = (predictions - targets)
+    total += diff * diff
+  mean = total / len(predictions)
+  return mean
