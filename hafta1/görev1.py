a41698cc8d366de1835c79fe6b63bd055976ec72
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-08-24 07:00:53 +0300

    Fix neuron function syntax and logic, finished.

--- hafta1/görev1.py
+++ hafta1/görev1.py
@@ -1,5 +1,5 @@
-def neuron(){
+def neuron(inputsi weights, bias){
   total = 0
-    for range x w zip
-  total += bias
-return total
+    for range x, w in zip(inputs, weights)
+  total += x * w
+  return total
