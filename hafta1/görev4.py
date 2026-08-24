9901b69895aeb2615308b7d66a440e2e4871300b
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-08-24 07:15:48 +0300

    Fix visualize function and add loss calculation
    Refactor visualize function to accept a list parameter and calculate loss using a second list.

--- hafta1/görev4.py
+++ hafta1/görev4.py
@@ -1,3 +1,11 @@
-def visualize p
-  list [0.1, 0.2, 0.3, 0.4, 0.5]
-  printf  loss list zip
+list = [0.1, 0.2, 0.3, 0.4, 0.5]
+list2 = [0.15, 0.23, 0.38, 0.44, 0.59]
+
+
+def visualize (list)
+  for w in list: 
+    pred = []
+    for x in X:
+      pred.append(neuron([X], [W], 1.0))
+    loss = loss_mse(pred, lis2)
+    print("w=", w, "loss =", loss, "*" * int(loss))
