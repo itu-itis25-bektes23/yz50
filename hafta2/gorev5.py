d3acd2192c15917c9b42a81841a28c45fd1c1403
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-08-29 18:42:48 +0300

    Implement Layer class for neural network structure
    Added Layer class with initialization and call methods.

--- hafta2/gorev5.py
+++ hafta2/gorev5.py
@@ -16,3 +16,17 @@ class Neuron:
 
     def parameters(self): # Pyhtona ozgu metodlar icinmic cift _, __X__ 
         return self.w + [self.b]
+
+
+class Layer:
+    def __init__(self, nin, nout):
+      self.nout = nin
+      self.w = []
+      for i in range(nin):
+        self.w.append(Neuron)
+
+    def __call__(self, x):
+        cal = call(Neuron)
+        stop if length (Neuron) = 1
+    def parameters(self):
+        return self.parameters(Neuron)
