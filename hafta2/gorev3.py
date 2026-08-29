b324c7c9c24484bce0cb815504839e0854fed0c4
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-08-29 17:49:06 +0300

    Refactor Value class and implement backward method
    Refactor Value class methods and add backward propagation.

--- hafta2/gorev3.py
+++ hafta2/gorev3.py
@@ -1,40 +1,46 @@
 class Value:
-  def __init__(self,data, prev = (), op = ''):
-          self.data = data
-          self.grad = 0.0
-          self._prev = set(prev)
-          self._op = op
-          self._backward = lambda: None
-    def __add__(self, other):
-      out = Value(self.data + other.data, (self, other), '+')
-        def _backward():
-            self.grad += self.data * out.grad
-            other.grad += out.grad * self.data
-        out._backward = _backward
-        return out
-      out = Value(self.data + other.data, (self, other), '+')
-    def __mul__(self, other):
-        def _backward():
-            self.grad += other.grad * self.grad
-            other.grad += other.grad + self.grad
-        out._backward = _backward
-        return out
-      return Value(self.data * other.data, (self, other), '*')
-
-a = Value(2.0)
-b = Value(-3.0)
-c = Value(10.0)
-d = a * b + c
-print(d.data, d._op)
+    def __init__(self, data, prev=(), op=''):
+        self.data = data
+        self.grad = 0.0
+        self._prev = set(prev)
+        self._op = op
+        self._backward = lambda: None
 
     def __repr__(self):
         return f"Value(data={self.data}, grad={self.grad})"
-a = Value(2.0)
-b = Value(-3.0)
-c = Value(10.0)
-e = a * b
-d = e + c
-f = Value(-2.0)
-L = d * f
 
-print(L)
+    def __add__(self, other):
+        out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad  += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
+
+        return out
+
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other), '*')
+
+        def _backward():
+            self.grad  += ???  * out.grad
+            other.grad += ???  * out.grad
+        out._backward = _backward
+
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def build(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build(child)
+                topo.append(v)
+        build(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
