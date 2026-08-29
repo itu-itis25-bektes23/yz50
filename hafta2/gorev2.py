4dc2ea9fc224e7502cb31ebfcf4a1a898d9636b8
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-08-29 18:21:00 +0300

    Refactor Value class initialization and methods

--- hafta2/gorev2.py
+++ hafta2/gorev2.py
@@ -1,22 +1,19 @@
 class Value:
-  def __init__(self,data, prev = (), op = ''):
-          self.data = data
-          self.grad = 0.0
-          self._prev = set(prev)
-          self._op = op
-    def __add__(self, other):
-      return Value(self.data + other.data, (self, other), '+')
-    def __mul__(self, other):
-      return Value(self.data * other.data, (self, other), '*')
-
-a = Value(2.0)
-b = Value(-3.0)
-c = Value(10.0)
-d = a * b + c
-print(d.data, d._op)
+    def __init__(self, data, prev=(), op=''):
+        self.data = data
+        self.grad = 0.0
+        self._prev = set(prev)
+        self._op = op
 
     def __repr__(self):
         return f"Value(data={self.data}, grad={self.grad})"
+
+    def __add__(self, other):
+        return Value(self.data + other.data, (self, other), '+')
+
+    def __mul__(self, other):
+        return Value(self.data * other.data, (self, other), '*')
+      
 a = Value(2.0)
 b = Value(-3.0)
 c = Value(10.0)
