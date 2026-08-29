30c17b721456a2aa9ffaeab02e873e36fbca26d0
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-08-29 17:41:11 +0300

    Implement calculations with Value class
    Added calculations using Value instances and printed the result.

--- hafta2/gorev2.py
+++ hafta2/gorev2.py
@@ -17,3 +17,12 @@ print(d.data, d._op)
 
     def __repr__(self):
         return f"Value(data={self.data}, grad={self.grad})"
+a = Value(2.0)
+b = Value(-3.0)
+c = Value(10.0)
+e = a * b
+d = e + c
+f = Value(-2.0)
+L = d * f
+
+print(L)
