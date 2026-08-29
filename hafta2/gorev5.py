08a9517ad409f8a7d554324ca90eeedd604f5701
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-08-29 18:33:58 +0300

    Correct for loop syntax and indentation in Neuron class
    Fix syntax error in for loop and adjust indentation.

--- hafta2/gorev5.py
+++ hafta2/gorev5.py
@@ -6,7 +6,11 @@ class Neuron:
     def __init__(self, nin):
       self.b = Value( random.uniform(-1,1))
       self.w = []
-      for i in range (nin)
+      for i in range(nin):
         self.w.append(Value(random.uniform(-1,1)))
-
+    def __call__(self,x):
+        act = self.b
+        for wi, xi in zip(self.w, x):
+            act = act + wi*xi
+    return act.tanh()
       
