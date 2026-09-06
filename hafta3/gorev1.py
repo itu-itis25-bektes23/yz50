9111e298ad306506878464f1e945b08b5cadf897
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-09-06 17:29:39 +0300

    Fix syntax errors and improve code formatting

--- hafta3/gorev1.py
+++ hafta3/gorev1.py
@@ -1,6 +1,6 @@
 words = open('names.txt', 'r').read().splitlines()words[:10]
 
-for w in words[:1]
+for w in words[:1]:
   chs = ['<S>'] + list(w) + ['<E>']
   for ch1, ch2 in zip(chs, chs[1:]):
     bigram = (ch1, ch2)
@@ -9,7 +9,7 @@ import torch
 N = torch.zeros((28,28) , dtype=torch.int32)
 chars = sorted(list(set(''.join(words))))
 stoi = {s:i for i, s in enumerate(chars)}
-stoi{'<S>'} = 26
-stoi{'<E>'} = 27
+stoi['<S>'] = 26
+stoi['<E>'] = 27
 
 itos = {i:s for s,i in stoi.items() }
