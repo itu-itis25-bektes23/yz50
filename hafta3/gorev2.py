844323697dc091f59da768777d67d58e1c01b00e
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-09-06 17:55:23 +0300

    Add bigram analysis and plot for names
    Implement bigram frequency calculation and visualization.

---
+++ hafta3/gorev2.py
@@ -0,0 +1,36 @@
+words = open('names.txt', 'r').read().splitlines()
+words[:10]
+b = {}
+for w in words[:1]:
+    chs = ['.'] + list(w) + ['.'] #    chs = ['<S>'] + list(w) + ['<E>']
+#
+    for ch1, ch2 in zip(chs, chs[1:]):
+      bigram = (ch1, ch2)
+      b[bigram] = b.get(bigram, 0) + 1
+import torch
+N = torch.zeros((27,27) , dtype=torch.int32) #28#
+chars = sorted(list(set(''.join(words))))
+stoi = {s:i for i, s in enumerate(chars)}
+stoi['.'] = 26
+#stoi['<S>'] = 26#
+##stoi['<E>'] = 27##
+
+
+for w in words:
+    chs = ['.'] + list(w) + ['.'] ##chs = ['<S>'] + list(w) + ['<E>']
+    for ch1, ch2 in zip(chs, chs[1:]):
+      N[stoi[ch1], stoi[ch2]] += 1
+
+itos = {i:s for s,i in stoi.items() }
+import matplotlib.pyplot as plt
+plt.figure(figsize=(16,16))
+for i in range(27): #28
+    for j in range(27): #28
+        plt.text(j , i + 0.25, N[i,j].item(), ha="center") 
+        plt.text(j, i, itos[i] + itos[j], ha="center") 
+
+plt.imshow(N, cmap='Blues')
+plt.axis('off')
+P = N.float()
+for i in range(26):
+  N[i, _] =N[i, _] /  P.sum(1)
