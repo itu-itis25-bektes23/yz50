009d90388b3e3d084761a9a4b79aea990702f394
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-09-06 18:13:31 +0300

    Update bigram logic and likelihood calculation
    Refactor bigram calculation and log likelihood computation.

--- hafta3/gorev3.py
+++ hafta3/gorev3.py
@@ -2,8 +2,7 @@ words = open('names.txt', 'r').read().splitlines()
 words[:10]
 b = {}
 for w in words[:1]:
-    chs = ['.'] + list(w) + ['.'] #    chs = ['<S>'] + list(w) + ['<E>']
-#
+    chs = ['.'] + list(w) + ['.'] #    chs = ['<S>'] + list(w) + ['<E>']#
     for ch1, ch2 in zip(chs, chs[1:]):
       bigram = (ch1, ch2)
       b[bigram] = b.get(bigram, 0) + 1
@@ -19,15 +18,22 @@ stoi['.'] = 26
 for w in words:
     chs = ['.'] + list(w) + ['.'] ##chs = ['<S>'] + list(w) + ['<E>']
     for ch1, ch2 in zip(chs, chs[1:]):
-      log_likelihood += torch.log(P[stoi[ch1], stoi[ch2]])
-      n++
-avg_NLL =-log_likelihood / n
+      N[stoi[ch1], stoi[ch2]] += 1
 itos = {i:s for s,i in stoi.items() }
 
 P = N.float()
 ##for i in range(27):##
    ##P[i]= P[i] / P[i].sum()##
 P = P / P.sum(1, keepdim=True)
+
+
+for w in words:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+      log_likelihood += torch.log(P[stoi[ch1], stoi[ch2]])
+      n += 1
+avg_NLL =-log_likelihood / n
+
 
 g = torch.Generator().manual_seed(2147483647)
 
