ff5e23a7b2510c876756a46b3738100e64777581
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-09-06 17:37:26 +0300

    Fix bigram counting logic in gorev1.py

--- hafta3/gorev1.py
+++ hafta3/gorev1.py
@@ -16,7 +16,6 @@ stoi['<E>'] = 27
 for w in words:
     chs = ['<S>'] + list(w) + ['<E>']
     for ch1, ch2 in zip(chs, chs[1:]):
-      bigram = (stoi(ch1), stoi(ch2))
-      b[bigram] += 1
+      N[stoi[ch1], stoi[ch2]] += 1
 
 itos = {i:s for s,i in stoi.items() }
