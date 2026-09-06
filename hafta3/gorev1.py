39d6182248ccf98649ecc36308306c294594050f
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-09-06 08:24:40 +0300

    Implement bigram counting from names.txt
    Fix syntax errors and add bigram counting logic.

--- hafta3/gorev1.py
+++ hafta3/gorev1.py
@@ -1,2 +1,8 @@
 words = open('names.txt', 'r').read.splitlines()
 words[:10]
+
+for w in words[:1]
+  chs = ['<S>'] + list(w) + ['<E>']
+  for ch1, ch2 in zip(chs, chs[1:]):
+    bigram = (ch1, ch2)
+    b[bigram] = b.get(bigram, 0) + 1
