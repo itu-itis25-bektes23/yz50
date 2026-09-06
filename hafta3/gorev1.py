6a7f443c31a7a3f15454ce644ae94e1ef56dd4f1
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-09-06 17:51:45 +0300

    Fix matrix dimensions and character indices in gorev1.py
    Adjusted matrix size and character mappings for bigram counts.

--- hafta3/gorev1.py
+++ hafta3/gorev1.py
@@ -8,25 +8,24 @@ for w in words[:1]:
       bigram = (ch1, ch2)
       b[bigram] = b.get(bigram, 0) + 1
 import torch
-N = torch.zeros((28,28) , dtype=torch.int32)
+N = torch.zeros((27,27) , dtype=torch.int32) #28#
 chars = sorted(list(set(''.join(words))))
 stoi = {s:i for i, s in enumerate(chars)}
 stoi['.'] = 26
-#stoi['<S>'] = 26
-stoi['.'] = 27
-#stoi['<E>'] = 27
+#stoi['<S>'] = 26#
+##stoi['<E>'] = 27##
 
 
 for w in words:
-     chs = ['.'] + list(w) + ['.'] ##chs = ['<S>'] + list(w) + ['<E>']
+    chs = ['.'] + list(w) + ['.'] ##chs = ['<S>'] + list(w) + ['<E>']
     for ch1, ch2 in zip(chs, chs[1:]):
       N[stoi[ch1], stoi[ch2]] += 1
 
 itos = {i:s for s,i in stoi.items() }
 import matplotlib.pyplot as plt
 plt.figure(figsize=(16,16))
-for i in range(28):
-    for j in range(28):
+for i in range(27): #28
+    for j in range(27): #28
         plt.text(j , i + 0.25, N[i,j].item(), ha="center") 
         plt.text(j, i, itos[i] + itos[j], ha="center") 
 
