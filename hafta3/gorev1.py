2d04ef58c9ec0a16198d0bc0260419f6c38d9455
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-09-06 17:32:58 +0300

    Implement bigram counting for words
    Add bigram counting for words in the input list.

--- hafta3/gorev1.py
+++ hafta3/gorev1.py
@@ -13,4 +13,10 @@ stoi = {s:i for i, s in enumerate(chars)}
 stoi['<S>'] = 26
 stoi['<E>'] = 27
 
+for w in words:
+    chs = ['<S>'] + list(w) + ['<E>']
+    for ch1, ch2 in zip(chs, chs[1:]):
+      bigram = (ch1, ch2)
+      b[bigram] = b.get(bigram, 0) + 1
+
 itos = {i:s for s,i in stoi.items() }
