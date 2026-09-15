8563f47edf05cd6787e530fb440d904ec6c2aa08
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-09-15 06:15:50 +0300

    Update training loop to run for 10000 iterations
    Increase training iterations and fix random index generation.

--- hafta4/gorev3.py
+++ hafta4/gorev3.py
@@ -45,11 +45,10 @@ for p in parameters:
     p.requires_grad = True
 
 lr = 0.1
-for i in range(100):
-  for i in range 32:
-    ix = torch.randint((0, X.shape[0]), 32)
-    X_slice = X[ix]
-    Y_slice = Y[ix]
+for i in range(10000):
+  ix = torch.randint(0, X.shape[0], 32, generator=g)
+  X_slice = X[ix]
+  Y_slice = Y[ix]
   emb_slice = C[X_slice]
   emb_slice = emb_slice.view(emb_slice.shape[0], -1)
   h = torch.tanh(emb_slice @ W1 + b1)
