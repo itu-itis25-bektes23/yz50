56f123394f81c89202b61de25f868aec04d45cdd
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-09-15 06:26:18 +0300

    Modify learning rate and include matplotlib for plotting
    Updated learning rate handling and added plotting.

--- hafta4/gorev3.py
+++ hafta4/gorev3.py
@@ -1,9 +1,10 @@
 #MLP Language Model
 import torch
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
 # --- veriyi oku ---
-words = open('isimler.txt').read().splitlines()      # dosyadan satır satır, boşlukları temizle
+words = open('names.txt').read().splitlines()      # dosyadan satır satır, boşlukları temizle
 print(len(words), words[:5])
 
 # --- alfabe ---
@@ -43,8 +44,8 @@ b2 = torch.randn(vocab_size, generator=g)
 parameters = [C, W1, b1, W2, b2]
 for p in parameters:
     p.requires_grad = True
-
-lr = 0.1
+lrs = torch.linspace(-3, 0, 1000)
+lr = lrs
 for i in range(10000):
   ix = torch.randint(0, X.shape[0], (32,), generator=g)
   X_slice = X[ix]
@@ -61,3 +62,6 @@ for i in range(10000):
     p.data += -lr * p.grad
   if i % 1000 == 0:
     print(i, loss.item())
+
+plt.plot(lre[i], loss.item())
+plt.show()
