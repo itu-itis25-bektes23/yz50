32e3c5e73b090a2b0ccf27d0964500dd342720a8
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-09-20 14:28:01 +0300

    Add comparison for counts_sum in gorev1-2.py

--- hafta5/gorev1-2.py
+++ hafta5/gorev1-2.py
@@ -119,8 +119,10 @@ dlogprobs = torch.zeros(logprobs.grad.shape)
 dlogprobs[range(n), Yb] = -1.0/n
 dprobs = (1.0 / probs) * dlogprobs
 dcounts_sum_inv = (counts * dprobs).sum(dim=1, keepdim=True)
+dcounts_sum = (-counts_sum ** -2) * dcounts_sum_inv
 cmp('logprobs', dlogprobs, logprobs)
 cmp('probs', dprobs, probs)
 cmp('counts_sum_inv ', dcounts_sum_inv, counts_sum_inv )
+cmp('counts_sum', dcounts_sum, counts_sum)
 
 
