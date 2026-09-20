5ddf29b466e5e7d13c1b2816c33557a17c9f643d
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-09-20 14:41:28 +0300

    Add computation for dlogits using logit_maxes

--- hafta5/gorev1-2.py
+++ hafta5/gorev1-2.py
@@ -133,3 +133,6 @@ cmp('counts', dcounts, counts)
 dnorm_logits = counts * dcounts
 cmp('norm_logits', dnorm_logits, norm_logits)
 dlogit_maxes = -logit_maxes.sum(dim=1, keepdim=True)
+cmp('logit_maxes', dlogit_maxes, logit_maxes)
+dlogits = dnorm_logits + F.one_hot(logits.max(1).indices, num_classes=logits.shape[1]) * dlogit_maxes
+
