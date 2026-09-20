7e83bdde698cb8a4067a996e517e943fc0c8dc77
itu-itis25-bektes23 <bektes23@itu.edu.tr>
2026-09-20 07:32:01 +0300

    Fix indexing and reshape for training and dev sets

--- hafta4/gorev3.py
+++ hafta4/gorev3.py
@@ -85,14 +85,14 @@ for i in range(30000):
 plt.plot(lossi)
 plt.show()
 with torch.no_grad():
-    emb_tr =  C[xtr]          # C indexed by Xtr
-    flat_tr = emb_slice.view(emb_tr.shape[0], -1)         # .view(...)
-    h_tr = torch.tanh(emb_tr @ W1 + b1)            # tanh
+    emb_tr =  C[Xtr]          # C indexed by Xtr
+    flat_tr = emb_tr.view(emb_tr.shape[0], -1)         # .view(...)
+    h_tr = torch.tanh(flat_tr @ W1 + b1)            # tanh
     logits_tr = h_tr @ W2 + b2
     print('train', F.cross_entropy(logits_tr, Ytr).item())
 
-emb_tr =  C[xdev]          # C indexed by Xtr
-flat_dev = emb_slice.view(emb_dev.shape[0], -1)         # .view(...)
-h_dev = torch.tanh(emb_dev @ W1 + b1)            # tanh
-logits_dev = h_dev @ W2 + b2
-print('dev', F.cross_entropy(logits_dev, Ydev).item())
+    emb_dev =  C[Xdev]          # C indexed by Xdev
+    flat_dev = emb_dev.view(emb_dev.shape[0], -1)         # .view(...)
+    h_dev = torch.tanh(flat_dev @ W1 + b1)            # tanh
+    logits_dev = h_dev @ W2 + b2
+    print('dev', F.cross_entropy(logits_dev, Ydev).item())
